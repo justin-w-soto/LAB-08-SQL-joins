f79f23250db8071ee8298583a67f07cf4c718eaa
Justin W Soto <soto@pdx.edu>
2021-08-17 09:06:43 -0700

    starting over

--- data/friends.js
+++ data/friends.js
@@ -3,7 +3,8 @@ module.exports = [
     
     name: 'turkey neck',
     cool_factor: 3,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 1
     
   },
 
@@ -11,41 +12,47 @@ module.exports = [
     
     name: 'crab claw',
     cool_factor: 4,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 2
   },
 
   {
    
     name: 'ham fist',
     cool_factor: 4,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 1
   },
 
   {
     
     name: 'block head',
     cool_factor: 10,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 2
   },
 
   {
     
     name: 'poindexter',
     cool_factor: 1,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 3
   },
 
   {
     
     name: 'grandma',
     cool_factor: 10,
-    cool_haircut: true
+    cool_haircut: true,
+    shirt_color_id: 4
   },
 
   {
     
     name: 'Gator',
     cool_factor: 8,
-    cool_haircut: false
+    cool_haircut: false,
+    shirt_color_id: 5
   }
 ];
